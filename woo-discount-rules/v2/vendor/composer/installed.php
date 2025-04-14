441d9f833a421f8e270c6c4f81dbeabc993d3d7d
Sean Murphy <sean@iamseanmurphy.com>
2025-04-14 14:18:31 +0000

    2.6.9

--- woo-discount-rules/v2/vendor/composer/installed.php
+++ woo-discount-rules/v2/vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '2.6.7.x-dev',
-        'version' => '2.6.7.9999999-dev',
+        'pretty_version' => '2.6.9.x-dev',
+        'version' => '2.6.9.9999999-dev',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => '3d3fd3315d0c1c50d4061ddc26a04b7ea9ffb878',
+        'reference' => 'b5ce4176b15230b92faf95986ae37f0975f429db',
         'name' => 'flycart/woo_discount_rules_v2',
         'dev' => false,
     ),
     'versions' => array(
         'flycart/woo_discount_rules_v2' => array(
-            'pretty_version' => '2.6.7.x-dev',
-            'version' => '2.6.7.9999999-dev',
+            'pretty_version' => '2.6.9.x-dev',
+            'version' => '2.6.9.9999999-dev',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => '3d3fd3315d0c1c50d4061ddc26a04b7ea9ffb878',
+            'reference' => 'b5ce4176b15230b92faf95986ae37f0975f429db',
             'dev_requirement' => false,
         ),
         'vlucas/valitron' => array(
